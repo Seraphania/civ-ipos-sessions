216b760bf305f6f688092671e5e5f5165d24bcea
“Seraphania” <strings333@hotmail.com>
2026-02-05 20:38:22 +0800

    Week 1 Complete

---
+++ week1/bottles_of_beer/bottles_of_beer.py
@@ -0,0 +1,8 @@
+n = 99
+while n > 1:
+    print(f"{n} bottles of beer on the wall, {n} bottles of beer,\nTake one down, pass it around, {n-1} bottles of beer on the wall.\n")
+    n = n-1
+    
+print(f"1 bottle of beer on the wall, 1 bottle of beer,\nTake one down, pass it around, no more bottles of beer on the wall.\n")
+
+print(f"No more bottles of beer on the wall, no more bottles of beer.\nTGo to the store and buy some more, 99 bottles of beer on the wall!\n")
